3affb035204ce012e7dc4b62ea5d96589ebea3b7
aymericthomine <67975592+aymericthomine@users.noreply.github.com>
2021-02-12 16:38:53 +0100

    Delete resources directory

--- resources/views/header.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="fixed-top container-fluid" style="margin-top: 20px; text-align:center;">
-            <img src="/images/header/listy_header.png" style="width:125px; margin:auto;">
-</div>
